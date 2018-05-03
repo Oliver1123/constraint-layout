6d386590ed5c71d97509213f664ce4515a210bcf
Oleg Zolotar <zolotar1123@gmail.com>
2018-05-03 11:54:01 +0300

    play with constraintsSet animation

--- constraint-layout-start/app/src/main/java/com/google/googleio/MainActivity.kt
+++ constraint-layout-start/app/src/main/java/com/google/googleio/MainActivity.kt
@@ -21,17 +21,63 @@ import android.support.constraint.ConstraintLayout
 import android.support.constraint.ConstraintSet
 
 import android.support.v7.app.AppCompatActivity
+import android.transition.ChangeBounds
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main_done.*
+import android.transition.TransitionManager
+import android.view.animation.AnticipateOvershootInterpolator
+import android.view.animation.OvershootInterpolator
+
 
 class MainActivity : AppCompatActivity() {
+    private var showComponents = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_done)
 
-        upload.setOnClickListener { Toast.makeText(applicationContext, "test", Toast.LENGTH_SHORT).show() }
+        upload.setOnClickListener {
+//            favoriteIconAnimation(showComponents)
+            triggerComponentsAnimation(showComponents)
+            showComponents = !showComponents
+        }
+
+    }
+
+    private fun favoriteIconAnimation(show: Boolean) {
+        val set = ConstraintSet()
+
+        // copy constraints settings from current ConstraintLayout to set
+        set.clone(activity_main_done)
+
+        // change constraints settings
+        set.clear(R.id.favorite, ConstraintSet.RIGHT)
+        set.clear(R.id.favorite, ConstraintSet.LEFT)
+        if (show) {
+            set.connect(R.id.favorite, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0)
+        } else {
+            set.connect(R.id.favorite, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0)
+        }
+        // enable animation
+        TransitionManager.beginDelayedTransition(activity_main_done)
+
+        // apply constraints settings from set to current ConstraintLayout
+        set.applyTo(activity_main_done)
+    }
+
+    private fun triggerComponentsAnimation(showComponents: Boolean){
+        val constraint = findViewById<ConstraintLayout>(R.id.activity_main_done)
+        val constraintSet = ConstraintSet()
+        val layoutID = if (showComponents)  R.layout.activity_main_done else R.layout.activity_main_done_ext
+        constraintSet.clone(this, layoutID)
+
+        val transition = ChangeBounds()
+        transition.interpolator = OvershootInterpolator(1.0f)
+        transition.duration = 750
+
+        TransitionManager.beginDelayedTransition(constraint, transition)
+        constraintSet.applyTo(constraint)
     }
 }
